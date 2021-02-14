ffcdf7ee96a601c9a85ba756f65f8dd67c25e425
rajanpawar90 <rajanpawar90@gmail.com>
2021-02-14 13:07:28 -0800

    c

--- textClassificationWithTfHub.py
+++ textClassificationWithTfHub.py
@@ -18,3 +18,36 @@ train_data, validation_data, test_data = tfds.load(
 train_examples_batch, train_labels_batch = next(iter(train_data.batch(10)))
 train_examples_batch
 train_labels_batch
+
+#Build the model
+embedding = "https://tfhub.dev/google/nnlm-en-dim50/2"
+hub_layer = hub.KerasLayer(
+    embedding, 
+    input_shape=[],
+    dtype=tf.string,
+    trainable=True)
+hub_layer(train_examples_batch[:3])
+
+model = tf.keras.Sequential()
+model.add(hub_layer)
+model.add(tf.keras.layers.Dense(16, activation='relu'))
+model.add(tf.keras.layers.Dense(1))
+model.summary()
+
+model.compile(optimizer='adam',\
+    loss=tf.keras.losses.BinaryCrossentropy(from_logits=True),
+    metrics=['accuracy'])
+
+#train the model
+history = model.fit(train_data.shuffle(10000).batch(512),\
+    epochs=10,
+    validation_data=validation_data.batch(512),
+    verbose=1)
+
+#evaluate model
+
+results = model.evaluate(test_data.batch(512), verbose=2)
+
+for name, value in zip(model.metrics_names, results):
+  print("%s: %.3f" % (name, value))
+
