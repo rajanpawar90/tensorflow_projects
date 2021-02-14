3b737be3dc256ee7470c324fff0daceab5675c85
rajanpawar90 <rajanpawar90@gmail.com>
2021-02-14 11:46:16 -0800

    commit

--- textClassificationWithTfHub.py
+++ textClassificationWithTfHub.py
@@ -9,6 +9,12 @@ print("Eager mode: ", tf.executing_eagerly())
 print("Hub version: ", hub.__version__)
 print("GPU is", "available" if tf.config.list_physical_devices("GPU") else "NOT AVAILABLE")
 
-train_data, validation_data, test_data = tfds.load(name="imdb_reviews",
-                                                   split=('train[:60', 'train[60%:]', 'test'),
-                                                   as_supervised=True)
+train_data, validation_data, test_data = tfds.load(
+    name="imdb_reviews", 
+    split=('train[:60%]', 'train[60%:]', 'test'),
+    as_supervised=True)
+
+#Explore the data
+train_examples_batch, train_labels_batch = next(iter(train_data.batch(10)))
+train_examples_batch
+train_labels_batch
